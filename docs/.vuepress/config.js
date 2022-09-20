3a4e21b10d7eca79bf8842b13b84f8bc6facecc7
Jérémie LITZLER <2410556+JeremieLitzler@users.noreply.github.com>
2022-09-20 07:53:21 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const version = '1.0.169';
+const version = '1.0.174';
 const { path } = require('@vuepress/utils');
 const { vuePressThemeJeremieL } = require('./theme');
 const { viteBundler } = require('@vuepress/bundler-vite');
