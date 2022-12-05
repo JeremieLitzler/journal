fa66fcf7c28a9716423c2317a9de1f991d260a5f
Jeremie Litzler <jeremie.litzler@gmail.com>
2022-12-05 07:34:57 +0100

    disable imageLazyLoad

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -34,7 +34,7 @@ export default hopeTheme({
       blog: true,
       mdEnhance: {
         imageTitle: true,
-        imageLazyload: true
+        // imageLazyload: true
       },
       /*pwa: {
         cacheHTML: true,
